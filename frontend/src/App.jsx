43c0b620ba7b83e5a5f8f6610927256401ee2f31
DESKTOP-18UVIKD\MSI <pasindu.20191044@iit.ac.lk>
2025-05-06 15:00:56 +0530

    routes added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,17 @@
+import { BrowserRoute, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import AdminDashboard from "./pages/AdminDashboard";
+
 function App() {
   return (
     <>
-      <div className="text-3xl text-teal-500">Welcome to LeaveApp</div>
-      <div className="text-6xl text-red-500">I WILL DO THIS</div>
-      <div className="text-2xl text-blue-500">Let's get started!</div>
+      <BrowserRoute>
+        <Routes>
+          <Route path="/" element={<Navigate to="/admin-dashboard" />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/admin-dashboard" element={<AdminDashboard />}></Route>
+        </Routes>
+      </BrowserRoute>
     </>
   );
 }
