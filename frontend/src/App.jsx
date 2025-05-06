0392ae83a80379a9c0773918ed3680b206b2f58d
DESKTOP-18UVIKD\MSI <pasindu.20191044@iit.ac.lk>
2025-05-06 11:24:47 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="text-3xl text-teal-500">Welcome to LeaveApp</div>
+      <div className="text-6xl text-red-500">I WILL DO THIS</div>
+      <div className="text-2xl text-blue-500">Let's get started!</div>
+    </>
+  );
+}
+
+export default App;
