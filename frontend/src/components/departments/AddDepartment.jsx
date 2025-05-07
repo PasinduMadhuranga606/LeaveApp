c8a7573a78297798b45be45975e0b6c525540e0b
DESKTOP-18UVIKD\MSI <pasindu.20191044@iit.ac.lk>
2025-05-07 19:23:11 +0530

    variables created

--- frontend/src/components/departments/AddDepartment.jsx
+++ frontend/src/components/departments/AddDepartment.jsx
@@ -1,10 +1,46 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const AddDepartment = () => {
+  const [department, setDepartment] = useState({
+    dep_name: "",
+    description: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setDepartment({ ...department, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.prevetDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/api/department/add",
+        department,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+      if (response.data.success) {
+        navigate("/admin-dashboard/departments");
+      }
+    } catch (error) {
+      if (error.response && !error.response.data.success) {
+        alert(error.response.data.error);
+      }
+    }
+  };
+
   return (
     <div className="max-w-3xl mx-auto mt-10 bg-white p-8 rounded-md shadow-md w-96">
       <h3 className="text-2xl font-semibold mb-6">Add New Department</h3>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <label
             htmlFor="dep_name"
@@ -14,6 +50,8 @@ const AddDepartment = () => {
           </label>
           <input
             type="text"
+            name="dep_name"
+            onChange={handleChange}
             placeholder="Department Name"
             className="mt-1 w-full p-2 border border-gray-300 rounded-md"
             required
@@ -29,6 +67,7 @@ const AddDepartment = () => {
           </label>
           <textarea
             name="description"
+            onChange={handleChange}
             placeholder="Description"
             className="mt-1 p-2 block w-full border border-gray-300 rounded-md"
             rows="4"
