97377f8d6c6af50ca937243a5e9b1212ce11d5ee
DESKTOP-18UVIKD\MSI <pasindu.20191044@iit.ac.lk>
2025-05-08 18:46:23 +0530

    employee controller & routes files created

---
+++ server/controllers/employeeController.js
@@ -0,0 +1,17 @@
+const addEmployee = async (req, res) => {
+  //   try {
+  //     const { dep_name, description } = req.body;
+  //     const newDepartment = new Department({
+  //       dep_name,
+  //       description,
+  //     });
+  //     await newDepartment.save();
+  //     return res.status(200).json({ success: true, department: newDepartment });
+  //   } catch (error) {
+  //     return res
+  //       .status(500)
+  //       .json({ success: false, error: "add department server error" });
+  //   }
+};
+
+export { addEmployee };
