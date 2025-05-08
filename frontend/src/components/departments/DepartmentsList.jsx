84364b1cbc4a27fd4ef8527fee9de4844324ace9
DESKTOP-18UVIKD\MSI <pasindu.20191044@iit.ac.lk>
2025-05-08 08:16:04 +0530

    get department created

--- frontend/src/components/departments/DepartmentsList.jsx
+++ frontend/src/components/departments/DepartmentsList.jsx
@@ -1,29 +1,78 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import DataTable from "react-data-table-component";
+import { columns, DepartmentButtons } from "../../utils/DepartmentHelper";
+import axios from "axios";
 
 const DepartmentsList = () => {
+  const [departments, setDepartments] = useState([]);
+  const [depLoading, setDepLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchDepartments = async () => {
+      setDepLoading(true);
+      try {
+        const response = await axios.get(
+          "http://localhost:5000/api/department",
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          }
+        );
+        if (response.data.success) {
+          let sno = 1;
+          const data = await response.data.departments.map((dep) => ({
+            _id: dep._id,
+            sno: sno++,
+            dep_name: dep.dep_name,
+            action: <DepartmentButtons />,
+          }));
+          setDepartments(data);
+        }
+      } catch (error) {
+        if (error.response && !error.response.data.success) {
+          alert(error.response.data.error);
+        }
+      } finally {
+        setDepLoading(false);
+      }
+    };
+
+    fetchDepartments();
+  }, []);
+
   return (
-    <div className="p-5">
-      <div className="text-center">
-        <h3 className="text-3xl md:text-3xl font-semibold">
-          Manage Departments
-        </h3>
-      </div>
-      <div className="flex justify-between items-center">
-        <input
-          type="text"
-          placeholder="Search Department Name"
-          className="px-4 py-0.5 rounded-md border border-black bg-white"
-          //className="px-4 py-0.5 border"
-        />
-        <Link
-          to="/admin-dashboard/add-department"
-          className="px-6 py-1.5 rounded-md text-white border border-black bg-violet-700 hover:bg-violet-800 transition-colors duration-200"
-        >
-          Add New Department
-        </Link>
-      </div>
-    </div>
+    <>
+      {depLoading ? (
+        <div>Loading......</div>
+      ) : (
+        <div className="p-5">
+          <div className="text-center">
+            <h3 className="text-3xl md:text-3xl font-semibold">
+              Manage Departments
+            </h3>
+          </div>
+          <div className="flex justify-between items-center">
+            <input
+              type="text"
+              placeholder="Search Department Name"
+              className="px-4 py-0.5 rounded-md border border-black bg-white"
+              //className="px-4 py-0.5 border"
+            />
+            <Link
+              to="/admin-dashboard/add-department"
+              className="px-6 py-1.5 rounded-md text-white border border-black bg-violet-700 hover:bg-violet-800 transition-colors duration-200"
+            >
+              Add New Department
+            </Link>
+          </div>
+          <div>
+            <DataTable columns={columns} data={departments} />
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
--- server/controllers/departmentController.js
+++ server/controllers/departmentController.js
@@ -1,5 +1,16 @@
 import Department from "../models/Department.js";
 
+const getDepartments = async (req, res) => {
+  try {
+    const departments = await Department.find();
+    return res.status(200).json({ success: true, departments });
+  } catch (error) {
+    return res
+      .success(500)
+      .json({ success: false, error: "get department server error" });
+  }
+};
+
 const addDepartment = async (req, res) => {
   try {
     const { dep_name, description } = req.body;
@@ -16,4 +27,4 @@ const addDepartment = async (req, res) => {
   }
 };
 
-export { addDepartment };
+export { getDepartments, addDepartment };
