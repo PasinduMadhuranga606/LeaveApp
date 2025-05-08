aadee9773eb41cf282e9916585ad277c0483bcad
DESKTOP-18UVIKD\MSI <pasindu.20191044@iit.ac.lk>
2025-05-08 20:07:02 +0530

    add employee serverside created

--- server/controllers/employeeController.js
+++ server/controllers/employeeController.js
@@ -1,17 +1,71 @@
+import multer from "multer";
+import Employee from "../models/Employee.js";
+import User from "../models/User.js";
+import bcrypt from "bcrypt";
+import path from "path";
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage: storage });
+
 const addEmployee = async (req, res) => {
-  //   try {
-  //     const { dep_name, description } = req.body;
-  //     const newDepartment = new Department({
-  //       dep_name,
-  //       description,
-  //     });
-  //     await newDepartment.save();
-  //     return res.status(200).json({ success: true, department: newDepartment });
-  //   } catch (error) {
-  //     return res
-  //       .status(500)
-  //       .json({ success: false, error: "add department server error" });
-  //   }
+  try {
+    const {
+      name,
+      email,
+      employeeId,
+      dob,
+      gender,
+      maritalStatus,
+      designation,
+      department,
+      employmentType,
+      password,
+      role,
+    } = req.body;
+
+    const user = await User.findOne({ email });
+    if (user) {
+      return res
+        .status(400)
+        .json({ success: false, error: "user already registered in employee" });
+    }
+
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+      name,
+      email,
+      password: hashPassword,
+      role,
+      profileImage: req.file ? req.file.filename : "",
+    });
+    const savedUser = await newUser.save();
+
+    const newEmployee = new Employee({
+      userId: savedUser._id,
+      employeeId,
+      dob,
+      gender,
+      maritalStatus,
+      designation,
+      department,
+      employmentType,
+    });
+    await newEmployee.save();
+    return res.status(200).json({ success: true, message: "employee created" });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ success: false, error: "add employee server error" });
+  }
 };
 
-export { addEmployee };
+export { addEmployee, upload };
