1046b2508aae19ac41c470613862f66ae3151000
DESKTOP-18UVIKD\MSI <pasindu.20191044@iit.ac.lk>
2025-05-09 18:39:34 +0530

    logout function fixed

--- frontend/src/components/dashboard/Navbar.jsx
+++ frontend/src/components/dashboard/Navbar.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import { useAuth } from "../../context/authContext";
 
 const Navbar = () => {
-  const { user } = useAuth();
+  const { user, logout } = useAuth();
 
   //   console.log(user.name);
   return (
     <div className="flex items-center text-white justify-between h-15 bg-violet-500 px-6 shadow-md">
       <p className="text-lg font-medium">Welcome {user.name}</p>
-      <button className="px-4 py-1.5 rounded-md border border-white bg-violet-700 hover:bg-violet-800 transition-colors duration-200">
+      <button
+        className="px-4 py-1.5 rounded-md border border-white bg-violet-700 hover:bg-violet-800 transition-colors duration-200"
+        onClick={logout}
+      >
         Logout
       </button>
     </div>
