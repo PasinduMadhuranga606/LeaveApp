53f5960a5b79cd0b4db70f75371ff5f06cc472f7
DESKTOP-18UVIKD\MSI <pasindu.20191044@iit.ac.lk>
2025-05-09 19:03:04 +0530

    employee dashboard sidebar added

--- frontend/src/components/dashboard/EmployeeSidebar.jsx
+++ frontend/src/components/dashboard/EmployeeSidebar.jsx
@@ -1,7 +1,85 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import {
+  FaBuilding,
+  FaCalendarAlt,
+  FaCogs,
+  FaTachometerAlt,
+  FaUsers,
+} from "react-icons/fa";
 
 const EmployeeSidebar = () => {
-  return <div>EmployeeSidebar</div>;
+  return (
+    <div className="bg-violet-950 text-white h-screen fixed left-0 top-0 bottom-0 space-y-2 w-64">
+      <div className="bg-violet-500 h-15 flex items-center justify-center">
+        <h3 className="text-2xl text-center font-play">Leave App</h3>
+      </div>
+      <div>
+        <div className="pl-4 pr-4">
+          <NavLink
+            to="/employee-dashboard"
+            // className={({ isActive }) =>
+            //   `flex items-center space-x-5 px-3 py-2.5 rounded-md transition-colors duration-200 ${
+            //     isActive ? "bg-violet-700" : "hover:bg-violet-800"
+            //   }`
+            // }
+            className={({ isActive }) =>
+              `${
+                isActive ? "bg-violet-600 " : " "
+              } flex items-center space-x-5 block px-3 py-2.5 rounded-md hover:bg-violet-700 transition-colors duration-200`
+            }
+            end
+          >
+            <FaTachometerAlt />
+            <span>Dashboard</span>
+          </NavLink>
+        </div>
+
+        <div className="pl-4 pr-4">
+          <NavLink
+            to="/employee-dashboard/profile"
+            className={({ isActive }) =>
+              `${
+                isActive ? "bg-violet-600 " : " "
+              } flex items-center space-x-5 block px-3 py-2.5 rounded-md hover:bg-violet-700 transition-colors duration-200`
+            }
+          >
+            <FaUsers />
+            <span>My Profile</span>
+          </NavLink>
+        </div>
+
+        <div className="pl-4 pr-4">
+          <NavLink
+            to="/employee-dashboard/leaves"
+            // className={({ isActive }) =>
+            //   `flex items-center space-x-5 px-3 py-2.5 rounded-md transition-colors duration-200 ${
+            //     isActive ? "bg-violet-700" : "hover:bg-violet-800"
+            //   }`
+            // }
+            className={({ isActive }) =>
+              `${
+                isActive ? "bg-violet-600 " : " "
+              } flex items-center space-x-5 block px-3 py-2.5 rounded-md hover:bg-violet-700 transition-colors duration-200`
+            }
+          >
+            <FaBuilding />
+            <span>Leaves</span>
+          </NavLink>
+        </div>
+
+        <div className="pl-4 pr-4">
+          <NavLink
+            to="/employee-dashboard"
+            className="flex items-center space-x-5 block py-2.5 px-4 rounded"
+          >
+            <FaCogs />
+            <span>Settings</span>
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default EmployeeSidebar;
